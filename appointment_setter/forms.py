4d9d00dee58d54f34e690f4fee56981e3a5fe05d
Ella Mae <91706826+EllaMae05@users.noreply.github.com>
2022-01-03 21:18:20 +0800

    Update forms.py
    user registration form

--- appointment_setter/forms.py
+++ appointment_setter/forms.py
@@ -1,6 +1,18 @@
+from django.db.models import fields
 from django.forms import ModelForm
 from .models import Student_Appointment
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from appointment_setter import models
+
+
+class UserRegisterForm (UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','username', 'email', 'password1', 'password2' ]
 
 class StudentAppointmentForm (ModelForm):
     # student_req_docu = forms.CharField(#verbose_name= 'student_req_docu',
