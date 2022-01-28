4db6525fde6d6da19cb980dbbbebb8bf567153dc
Reymaria <rheignemarrie.sandagan@gsfe.tupcavite.edu.ph>
2022-01-28 12:48:34 +0800

    modified:   appointment_setter/__pycache__/__init__.cpython-310.pyc 	modified:   appointment_setter/__pycache__/admin.cpython-310.pyc 	modified:   appointment_setter/__pycache__/apps.cpython-310.pyc 	modified:   appointment_setter/__pycache__/decorators.cpython-310.pyc 	modified:   appointment_setter/__pycache__/forms.cpython-310.pyc 	modified:   appointment_setter/__pycache__/models.cpython-310.pyc 	modified:   appointment_setter/__pycache__/urls.cpython-310.pyc 	modified:   appointment_setter/__pycache__/views.cpython-310.pyc 	modified:   appointment_setter/admin.py 	modified:   appointment_setter/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   appointment_setter/migrations/__pycache__/__init__.cpython-310.pyc 	modified:   appointment_setter/templates/student_side/home.html 	modified:   appointment_setter/templates/users/register.html 	modified:   django_project/__pycache__/settings.cpython-310.pyc 	modified:   django_project/__pycache__/urls.cpython-310.pyc 	modified:   django_project/__pycache__/wsgi.cpython-310.pyc

--- appointment_setter/admin.py
+++ appointment_setter/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User_registration
 from .models import Student_Appointment
 
-admin.site.register(User_registration)
 admin.site.register(Student_Appointment)
 
